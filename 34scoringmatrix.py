ca431a7b5d891b073a8acbf279ae061c17a148b2
Madison An <madisonan@Madisons-MacBook-Pro-9.local>
2024-03-16 21:36:25 -0700

    unit 3

--- 34scoringmatrix.py
+++ 34scoringmatrix.py
@@ -2,14 +2,14 @@
 
 n = 'ACGT'
 for i in n:
-	print(' ', i, end='')
+	print('  ', i, sep=' ', end='')
 print( )
 
 for nt1 in n:
 	print(nt1, end=' ')
 	for nt2 in n:
 		if nt1 == nt2:
-			print('+1', end=' ')
+			print('+1', end='  ')
 		else:
-			print('-1', end=' ')
+			print('-1', end='  ')
 	print( )
